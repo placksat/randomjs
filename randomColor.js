bd25706c865c0c5ab7be781598987b8907edc56e
admin <admin@admin.com>
2023-12-20 20:30:03 -0500

    Initial commit

---
+++ randomColor.js
@@ -0,0 +1,15 @@
+// Function to generate a random color
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Function to change the background color of the body when called
+function changeBackgroundColor() {
+    const color = getRandomColor();
+    document.body.style.backgroundColor = color;
+}
